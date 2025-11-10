305ee9591ed6c1c57df4baf61e899ac3f992e10f
안상환 <vooo10@naver.com>
2025-11-10 12:55:30 +0900

    og 수정

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,20 @@ import ThemeInitializer from "./ThemeInitializer";
 export const metadata = {
   title: "React Micro Projects | Sanghwan An",
   description: "미니 프로젝트로 React를 만들며 익히는 실습 레포지토리",
+  openGraph: {
+    title: "React Micro Projects | Sanghwan An",
+    description: "React로 직접 만들어보며 성장하는 프론트엔드 미니 프로젝트",
+    url: "https://sanghwan-mini-project.netlify.app/",
+    siteName: "Sanghwan Frontend Lab",
+    images: [
+      {
+        url: "/ansanghwan_character.png",
+        alt: "Sanghwan Frontend Lab Open Graph Image",
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
